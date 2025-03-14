661d83590668a597ac7bc914561b63d8dd3eadb6
Lucas537 <ll6767248@gmail.com>
2025-03-13 21:39:11 -0300

    fim da aula

--- 04-Introducao-react-native/App.js
+++ 04-Introducao-react-native/App.js
@@ -1,20 +1,44 @@
+//Imports
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Image} from 'react-native';
 
+//Componentes principal
+//Ele deve retornar o que será rederizado na tela (Template feito com JSX)
 export default function App() {
+
+//Logica do meu componente
+
+const nome = "Lucas"
+
+function alerta() {
+  alert("Você Clicou no Botão")
+}
+
+
+
+  // Retorno com JSX
   return (
-    <View style={styles.container}>
-      <Text>Palmeiras roubou o São Paulo</Text>
-      <Text>Palmeiras roubou o São Paulo</Text>
-      <Text>Palmeiras roubou o São Paulo</Text>
-      <Text>Palmeiras roubou o São Paulo</Text>
-      <Text>Palmeiras roubou o São Paulo</Text>
-      <Text>Palmeiras roubou o São Paulo</Text>
-      <Text>Palmeiras roubou o São Paulo</Text>
-      <Text>Palmeiras roubou o São Paulo</Text>
-      <Text>Palmeiras roubou o São Paulo</Text>
-      <Text>Palmeiras roubou o São Paulo</Text>
-      <StatusBar style="auto" />
+    <View style={styles.container}>   
+    <StatusBar style="auto" />
+
+   {/* Comentário centro  */}
+ {/* Código Javascript */}
+   <Text>Palmeiras roubou o São Paulo</Text>
+   <Text>{ 2+2 }</Text>
+   <Text>{nome}</Text>
+  
+     <Button title='Alterar' onPress={alerta}></Button>
+
+    <Image 
+    source={{uri:'https://media.tenor.com/76hC1837BhEAAAAM/monkey-capucin.gif'}}
+    style={{
+      height: 300,
+      width: 300
+    }}
+
+
+
+    />
     </View>
   );
 }
