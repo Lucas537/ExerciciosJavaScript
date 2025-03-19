2a4144ef8b248758129ffc1bb78eceb99d3d1653
Lucas537 <ll6767248@gmail.com>
2025-03-19 20:38:35 -0300

    intervalo

--- 04-Introducao-react-native/App.js
+++ 04-Introducao-react-native/App.js
@@ -1,53 +1,78 @@
 //Imports
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button, Image} from 'react-native';
+import { StyleSheet, Text, View, Button, Image, ScrollView } from 'react-native';
 
 //Componentes principal
 //Ele deve retornar o que será rederizado na tela (Template feito com JSX)
 export default function App() {
 
-//Logica do meu componente
+  //Logica do meu componente
 
-const nome = "Lucas"
+  const nome = "Lucas"
 
-function alerta() {
-  alert("Você Clicou no Botão")
-}
+  function alerta() {
+    alert("Você Clicou no Botão")
+  }
 
 
 
   // Retorno com JSX
   return (
-    <View style={styles.container}>   
-    <StatusBar style="auto" />
 
-   {/* Comentário centro  */}
- {/* Código Javascript */}
-   <Text>Palmeiras roubou o São Paulo</Text>
-   <Text>{ 2+2 }</Text>
-   <Text>{nome}</Text>
+    <ScrollView>
+
+
+
   
-     <Button title='Alterar' onPress={alerta}></Button>
+    <View style={styles.container}>
+      <StatusBar style="auto" />
 
-    <Image 
-    source={{uri:'https://media.tenor.com/76hC1837BhEAAAAM/monkey-capucin.gif'}}
-    style={{
-      height: 300,
-      width: 300
-    }}
+      {/* Comentário centro  */}
+      {/* Código Javascript */}
+      <Text style={styles.textoGrande}>Palmeiras roubou o São Paulo</Text>
+      <Text style={styles.textoGrande}>Palmeiras roubou o São Paulo</Text>
+      <Text style={styles.textoGrande}>Palmeiras roubou o São Paulo</Text>
+      <Text style={styles.textoGrande}>Palmeiras roubou o São Paulo</Text>
+      <Text style={styles.textoGrande}>Palmeiras roubou o São Paulo</Text>
+      <Text>{2 + 2}</Text>
+      <Text>{nome}</Text>
+
+      <Button title='Alterar' onPress={alerta}></Button>
+
+      <Image
+        source={{ uri: 'https://media.tenor.com/76hC1837BhEAAAAM/monkey-capucin.gif' }}
+        style={{
+          height: 300,
+          width: 300
+        }}
+      />
 
 
-
-    />
+      <Image
+        source={require('./Imagens/image.png')}
+        style={{
+          height: 300,
+          width: 300
+        }}
+      />
     </View>
+    </ScrollView>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'blue',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 50
   },
+
+  textoGrande: {
+    fontSize: 50,
+    fontWeight: 900,
+    fontStyle: 'italic'
+  }
 });
